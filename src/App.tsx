72df824d3d8b405ced888a3b849084d759e698ad
xanDevEnjoyer <105408915+xan9266@users.noreply.github.com>
2025-10-19 18:13:35 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+import { Routes, Route } from 'react-router-dom'
 import Confirmation from '@pages/Confirmation'
 import SignatureForm from '@pages/SignatureForm'
 import Thanks from '@pages/Thanks'
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
-        <Routes>
-          <Route path="/confirmation" element={<Confirmation />} />
-          <Route path="/signature" element={<SignatureForm />} />
-          <Route path="/thanks" element={<Thanks />} />
-          <Route path="/" element={<Confirmation />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      <Routes>
+        <Route path="/confirmation" element={<Confirmation />} />
+        <Route path="/signature" element={<SignatureForm />} />
+        <Route path="/thanks" element={<Thanks />} />
+        <Route path="/" element={<Confirmation />} />
+      </Routes>
+    </div>
   )
 }
 
