735290d13e8371419fc7bcd4c28240ad3255821b
xanDevEnjoyer <105408915+xan9266@users.noreply.github.com>
2025-10-19 18:12:48 +0200

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {/* ✅ On définit ici le préfixe pour GitHub Pages */}
+    <BrowserRouter basename="/challenge-15">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
